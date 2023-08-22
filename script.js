f226c9dd59d5756a09abd8ba21c4fb2726c3b925
Yusuf Yıldız <yusufyldz318@gmail.com>
2023-08-22 17:05:52 +0300

    Update

---
+++ script.js
@@ -0,0 +1,25 @@
+function loadPage(pageName) {
+    const contentDiv = document.getElementById("content");
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", pageName, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            contentDiv.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navbarPlaceholder = document.getElementById("navbar-placeholder");
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "navbar.html", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            navbarPlaceholder.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+});
