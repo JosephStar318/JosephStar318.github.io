cffe79660000ba4cb86391fad72fe24b953ae7b5
Yusuf Yıldız <yusufyldz318@gmail.com>
2023-08-22 17:28:25 +0300

    testing new pages

--- script.js
+++ script.js
@@ -11,23 +11,15 @@ function loadPage(pageName) {
     xhr.send();
 }
 
-// Use a flag to track if the navbar has been loaded
-let navbarLoaded = false;
-
 document.addEventListener("DOMContentLoaded", function () {
     const navbarPlaceholder = document.getElementById("navbar-placeholder");
 
-    if (!navbarLoaded && navbarPlaceholder) {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", "navbar.html", true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                navbarPlaceholder.innerHTML = xhr.responseText;
-                navbarLoaded = true; // Set the flag to true
-            }
-        };
-        xhr.send();
-    } else {
-        console.error("Navbar placeholder element not found.");
-    }
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "navbar.html", true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            navbarPlaceholder.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
 });
